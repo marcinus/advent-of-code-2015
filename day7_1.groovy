2c6c9e5cfdde4a14f630abf6fe5ce45ae039bf2d
Marcin Szymura <marcin.szymura@gmail.com>
2024-12-27 03:31:45 +0100

    Day 7-1

---
+++ day7_1.groovy
@@ -0,0 +1,38 @@
+static short eval(vars, rules, String target) {
+    if (target.isNumber()) return (target as short)
+    if (vars.containsKey(target)) return vars[target] as short
+    String condition = rules[target]
+    if (condition.contains('AND')) {
+        def (A, B) = condition.split(' AND ').collect { eval(vars, rules, it) }
+        vars[target] = A & B
+    } else if (condition.contains(' OR ')) {
+        def (A, B) = condition.split(' OR ').collect { eval(vars, rules, it) }
+        vars[target] = A | B
+    } else if (condition.contains(' LSHIFT ')) {
+        def (A, B) = condition.split(' LSHIFT ').collect { eval(vars, rules, it) }
+        vars[target] = A << B
+    } else if (condition.contains(' RSHIFT ')) {
+        def (A, B) = condition.split(' RSHIFT ').collect { eval(vars, rules, it) }
+        vars[target] = A >> B
+    } else if (condition.startsWith('NOT ')) {
+        short A = eval(vars, rules, condition.split('NOT ')[1])
+        vars[target] = ~A
+    } else {
+        vars[target] = eval(vars, rules, condition)
+    }
+    vars[target] as short
+}
+
+static short solve(String input) {
+    def rules = (input.split('\n') as List).collectEntries { rule ->
+        def (LHS, target) = rule.split(' -> ')
+        [(target): LHS]
+    }
+    Map<String, Short> vars = [:]
+    rules.keySet().each {
+        eval(vars, rules, it)
+    }
+    vars['a']
+}
+
+println solve(new File('input/7.txt').text)
