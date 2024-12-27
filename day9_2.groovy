e923b24c008b2bd38f23d43a47c4d15ef3c362fe
Marcin Szymura <marcin.szymura@gmail.com>
2024-12-27 20:54:08 +0100

    Day 9-2

---
+++ day9_2.groovy
@@ -0,0 +1,41 @@
+static long eval(Map<String, Map<String, Long>> d, List<String> cycle) {
+    def prev = cycle.first()
+    cycle.drop(1).sum {
+        def segment = d[prev][it]
+        prev = it
+        segment
+    } as long
+}
+
+static long longestHamiltonCycle(Map<String, Map<String, Long>> d, List<String> current) {
+    if (current.size() == d.size()) {
+        return eval(d, current)
+    }
+    def visited = current as Set
+    d[current.last()].keySet().collect { i ->
+        if (!visited.contains(i)) {
+            return longestHamiltonCycle(d, current + i)
+        }
+        0L
+    }.max() as long ?: 0L
+}
+
+static long solve(String input) {
+    Map<String, Map<String, Long>> d = [:].withDefault { [:].withDefault { Long.MAX_VALUE } }
+    (input.split('\n') as List).each {
+        def (ij, delta) = it.split(' = ') as List
+        def (i, j) = ij.split(' to ') as List
+        d[i][j] = delta as long
+        d[j][i] = delta as long
+    }
+    d.keySet().collect {
+        longestHamiltonCycle(d, [it])
+    }.max() as long
+}
+
+def sampleInput = '''London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141'''
+
+assert 982L == solve(sampleInput)
+println solve(new File('input/9.txt').text)
