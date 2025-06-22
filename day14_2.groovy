d1887d1b0426716217f2eebd732790faf5b69007
Marcin Szymura <marcin.szymura@gmail.com>
2025-06-22 18:45:25 +0200

    Day 14-2

---
+++ day14_2.groovy
@@ -0,0 +1,48 @@
+def sampleInput = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+"""
+
+PATTERN = ~/^\S+ can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds.$/
+
+def parse(List<String> lines) {
+    def reindeer = []
+    lines.each { line ->
+        if (line ==~ PATTERN) {
+            def matcher = (line =~ PATTERN)[0]
+            reindeer << [
+                    velocity: matcher[1] as long,
+                    timeFly: matcher[2] as long,
+                    timeRest: matcher[3] as long
+            ]
+        } else {
+            throw new RuntimeException("Invalid regex match in ${line}")
+        }
+    }
+    reindeer
+}
+
+long solve(List<String> lines, long time = 1000) {
+    def reindeer = parse(lines)
+    reindeer.each { r -> r.points = 0 }
+    (1..time).each {
+        def reindeerToPoints = reindeer.collect { r ->
+            long totalTime = r.timeFly + r.timeRest
+            long fullCycles = (it / totalTime) as long
+            long partial = it % totalTime
+            if (partial > r.timeFly) {
+                partial = r.timeFly
+            }
+            r.currentDistance = (fullCycles * r.timeFly + partial) * r.velocity
+        }
+        def maxCurrentDistance = reindeer.collect { it.currentDistance }.max()
+        reindeer.findAll { it.currentDistance == maxCurrentDistance }.each { r ->
+            r.points += 1
+        }
+    }
+    reindeer.collect { it.points }.max()
+}
+
+
+assert 689L == solve(sampleInput.split('\n') as List<String>)
+
+println solve(new File('input/14.txt').readLines(), 2503)
