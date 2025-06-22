98fcc969dd5f33b947a28153ec26524479196fa8
Marcin Szymura <marcin.szymura@gmail.com>
2025-06-22 18:45:33 +0200

    Day 15-1

---
+++ day15_1.groovy
@@ -0,0 +1,69 @@
+def sampleInput = """Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+"""
+
+PATTERN = ~/^\S+: capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)$/
+
+def parse(List<String> lines) {
+    def ingrediens = []
+    lines.each { line ->
+        if (line ==~ PATTERN) {
+            def matcher = (line =~ PATTERN)[0]
+            ingrediens << [
+                    matcher[1] as long,
+                    matcher[2] as long,
+                    matcher[3] as long,
+                    matcher[4] as long,
+                    //matcher[5] as long
+            ]
+        } else {
+            throw new RuntimeException("Invalid regex match in ${line}")
+        }
+    }
+    ingrediens
+}
+
+long calculateCombination(ingredients, List vector) {
+    assert (vector.sum() ?: 0) == 100
+    def rows = ingredients.size()
+    def cols = ingredients[0].size()
+    def transposed = (0..<cols).collect { col ->
+        (0..<rows).collect { row ->
+            ingredients[row][col]
+        }
+    }
+
+    def multipliers = (0..<cols).collect { i ->
+        long sum = 0
+        transposed[i].eachWithIndex { long value, int j ->
+            sum += value * vector[j]
+        }
+        return sum >= 0 ? sum : 0
+    } // So we get the set of numbers
+    long score = multipliers.inject(1) { total, value ->
+        total * value
+    } as long
+    score
+}
+
+long bruteForce(ingredients, List soFar) {
+    if (soFar.size() == ingredients.size()) {
+        return calculateCombination(ingredients, soFar)
+    } else if (soFar.size() == ingredients.size() - 1) {
+        return bruteForce(ingredients, soFar + [100 - (soFar.sum() ?: 0)])
+    } else {
+        int remaining = 100 - (soFar.sum() ?: 0 as int)
+        long best = 0
+        for (int i = 0; i <= remaining; i++) {
+            best = Math.max(best, bruteForce(ingredients, soFar + [i]))
+        }
+        return best
+    }
+}
+
+long solve(input) {
+    bruteForce(parse(input), [])
+}
+
+assert 62842880L == solve(sampleInput.split('\n') as List<String>)
+println solve(new File('input/15.txt').readLines())
